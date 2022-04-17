c51f83fc2d5007ea9e9d2fe19cae66f2da5973cf
DanPark13 <dpark0703@gmail.com>
2022-04-17 09:46:29 -0400

    Add Event Handlers
    - Adds bot using its tag on console
    - Handle all events files

--- index.js
+++ index.js
@@ -6,19 +6,26 @@ const {token} = require('./config.json');
 // Create a new client instance
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
+// Get the commands into the collection
 client.commands = new Collection();
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Pettan Bot is Ready!');
-});
+// Get the event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+	}
+    else{
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // When taking in commands outlined in `deploy-commands`, interact with different responses
 client.on('interactionCreate', async interaction => {
